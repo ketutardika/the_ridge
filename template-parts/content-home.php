7e628303c86a20421c76ceb84dca13ac521b3fb0
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-01 15:34:19 +0800

    Update content-home.php

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -43,7 +43,17 @@
 <!-- End Section -->
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
+	<header class="entry-header">
+		<?php 
+			if (!is_elementor_editor()) {
+			    // Display the title using your desired format
+			    the_title('<h1 class="entry-title">', '</h1>');
+			}
+		?>
+	</header><!-- .entry-header -->
+
+	<?php the_ridge_post_thumbnail(); ?>
+
 	<div class="entry-content">
 		<?php
 		the_content();
