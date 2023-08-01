1b6f6266989bacafabe441f024659b69902c9688
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-08-01 15:47:17 +0800

    Update content-home.php

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -8,39 +8,6 @@
  */
 
 ?>
-<!-- Hero Start -->
-<section class="bg-half-170 bg-light pb-0 d-table w-100 bg-hero-ridge" style="background: url('wp-content/themes/the_ridge/source/assets/images/villa/bg_header_hero.png');">
-    <div class="container-fluid">
-        <div class="row justify-content-center mt-5">
-            <div class="col-lg-12 text-center">
-                <div class="title-heading mt-4">
-                    <h1 class="display-3 text-white title-dark mb-3"> Magnificent views </h1>
-                    <h5 class="fw-medium text-white title-dark mb-3 section-heading-text"> overlooking a tropical forest <br/> above the Ayung River </h5>   
-                </div>
-            </div><!--end col-->
-        </div><!--end row-->
-    </div> <!--end container-fluid-->
-</section><!--end section-->
-<!-- Hero End -->
-
-<!-- Start -->
-<section class="p-5 mt-5">
-    <div class="container-fluid">
-        <div class="row justify-content-center">
-            <div class="col-12">
-                <div class="text-center mb-4 pb-2">
-                    <h6 class="display-9 section-heading-text text-color-primary">THE COLLECTION</h6>
-                    <h4 class="display-5 mt-2 mb-3 text-color-secondary">Dolore Magna Aliqua</h4>
-                    <p class="display-8 mx-auto mb-0 text-color-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                    <div class="mt-4">
-                        <a href="javascript:void(0)" class="btn btn-ridge-primary display-9 ">FIND OUT MORE</a>
-                    </div>
-                </div>
-            </div><!--end col-->
-        </div><!--end row-->
-    </div>
-</section>
-<!-- End Section -->
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
